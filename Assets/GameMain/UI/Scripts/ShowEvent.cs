e9b27cfb839ee98a2ca058b9276ed59311e57662
Viola Huang <violahuang0904@163.com>
2024-05-02 17:10:38 +0800

    测试事件框架有问题

--- Assets/GameMain/UI/Scripts/ShowEvent.cs
+++ Assets/GameMain/UI/Scripts/ShowEvent.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using JKFrame;
 
 public class ShowEvent : MonoBehaviour
 {
     [SerializeField] private TMP_Text EventText;
+    [SerializeField] private Image EventImage;
 
     private void Start()
     {
         Records.Refresh += RefreshEvent;
+        EventText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        EventImage = transform.GetChild(1).GetComponent<Image>();
     }
-
 
     private void RefreshEvent()
     {
-        EventText.text = "事件 " + Records.EventCount;
+        EventText.text = ManagerVariant.CurrentGameEvent().Model.UIInfo.Title;
+
+        string ImageID = ManagerVariant.CurrentGameEvent().Model.UIInfo.TextureId;
+        EventImage.sprite = R.Texture.TestImage1_TestImage1;
     }
 }
--- Assets/JKFrame/R.cs
+++ Assets/JKFrame/R.cs
@@ -10,7 +10,13 @@ namespace R
     } 
     public static class Texture
     {
-
+ 
+        public static Texture2D TestImage1 { get => ResSystem.LoadAsset<Texture2D>("TestImage1"); }  
+        public static Sprite TestImage1_TestImage1 { get => ResSystem.LoadAsset<Sprite>("TestImage1[TestImage1]"); } 
+        public static Texture2D TestImage2 { get => ResSystem.LoadAsset<Texture2D>("TestImage2"); }  
+        public static Sprite TestImage2_TestImage2 { get => ResSystem.LoadAsset<Sprite>("TestImage2[TestImage2]"); } 
+        public static Texture2D TestImage3 { get => ResSystem.LoadAsset<Texture2D>("TestImage3"); }  
+        public static Sprite TestImage3_TestImage3 { get => ResSystem.LoadAsset<Sprite>("TestImage3[TestImage3]"); }
     } 
     public static class Test
     {
@@ -19,6 +25,15 @@ namespace R
         public static GameObject TestPropCard_GameObject(Transform parent = null,string keyName=null,bool autoRelease = true)
         {
             return ResSystem.InstantiateGameObject("TestPropCard", parent, keyName,autoRelease);
+        }
+    } 
+    public static class Card
+    {
+ 
+        public static GameObject ActionCard { get => ResSystem.LoadAsset<GameObject>("ActionCard"); }  
+        public static GameObject ActionCard_GameObject(Transform parent = null,string keyName=null,bool autoRelease = true)
+        {
+            return ResSystem.InstantiateGameObject("ActionCard", parent, keyName,autoRelease);
         }
     }
 }
