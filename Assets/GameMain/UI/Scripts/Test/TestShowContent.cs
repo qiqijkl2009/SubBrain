e9c0be6cae1cf724eccb079ad03a7967b387a0a0
Viola Huang <violahuang0904@163.com>
2024-05-04 01:40:04 +0800

    文字分页显示

--- Assets/GameMain/UI/Scripts/Test/TestShowContent.cs
+++ Assets/GameMain/UI/Scripts/Test/TestShowContent.cs
@@ -2,34 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class TestShowContent : MonoBehaviour
+public class TestShowContent : MonoBehaviour, IPointerClickHandler
 {
-    public TMP_Text ContentText;
+    public TMP_Text DescriptionContent;
     public int ClickCount = 0;
 
     void Start()
     {
-        if (ContentText == null) return;
+        DescriptionContent = GameObject.Find("Description").GetComponent<TextMeshProUGUI>();
+
+        if (DescriptionContent.text == null) return;
 
         //之后改成从事件里获取
         //ContentText.text = GameEventManager.CurrentGameEvent.Model.UIInfo.Content;
-        ContentText.text = "我慢慢地睁开了双眼,头痛欲裂。环顾四周,发现自己躺在一张陈旧的病床上。手碰到了一本破旧的笔记本和一支钢笔。我翻开笔记本,发现上面记载着一系列调查的笔记,而我的名字被写在最顶端——艾丽莎。";
+
+        DescriptionContent.text = "我慢慢地睁开了双眼,头痛欲裂。环顾四周,发现自己躺在一张陈旧的病床上。手碰到了一本破旧的笔记本和一支钢笔。我翻开笔记本,发现上面记载着一系列调查的笔记,而我的名字被写在最顶端——艾丽莎。";
 
     }
 
-    private void Update()
+    //切换事件时通知
+    void RefreshContent()
     {
-        if (Input.GetMouseButtonDown(0))
+        ClickCount = 0;
+        DescriptionContent.pageToDisplay = 1;
+        DescriptionContent.text = GameEventManager.CurrentGameEvent.Model.UIInfo.Content;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (ClickCount < DescriptionContent.textInfo.pageCount - 1)
         {
-            if (ClickCount < ContentText.textInfo.pageCount - 1)
-            {
-                ContentText.pageToDisplay += 1;
-                ClickCount++;
-                Debug.Log("文字翻页");
-            }
+            DescriptionContent.pageToDisplay += 1;
+            ClickCount++;
+        }
+        else
+        {
+            ClickCount = 0;
+            DescriptionContent.pageToDisplay = 1;
         }
     }
-
-    
 }
